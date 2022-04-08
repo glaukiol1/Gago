dfdbb295d8e3b205208e6e72c64359286767c6ca
glaukiol1 <glaukiol1@gmail.com>
2022-04-08 19:30:12 +0200

    changes name from parser to lexer

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/glaukiol1/gago/parser"
 )
 
+// https://jadmogaizel.medium.com/the-different-parts-of-writing-a-programming-language-b634711a6af5
 func main() {
 	parser.TestLex()
 }
