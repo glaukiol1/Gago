53f55a11c0b83d1667ddb5a0e0e70a7b71e44162
glaukiol1 <glaukiol1@gmail.com>
2022-04-10 00:26:26 +0200

    vm todo

---
+++ vm/vm.go
@@ -0,0 +1,17 @@
+package vm
+
+// vm | this package contains the virtual machine
+// for running gago code
+
+// TODO
+// there is a virtual memory map
+// which keeps track of variables and other things
+
+// TODO
+// there is a way to load modules,
+// and make their globals accessable
+
+// TODO
+// runs the AST, and is able to parse
+// and run the according functions
+// corresponding to the AST
