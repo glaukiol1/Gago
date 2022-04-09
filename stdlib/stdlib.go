fa5e474959d4bbbe8abae5216c5e941c0ccfd6b1
glaukiol1 <glaukiol1@gmail.com>
2022-04-10 01:27:47 +0200

    add stdlib todos

---
+++ stdlib/stdlib.go
@@ -0,0 +1,7 @@
+package stdlib
+
+// stdlib package for gago
+
+// this will export all the modules included in the stdlib
+// it will help with the creation of the VM since the stdlib
+// modules can be imported executed easily
