784b2703cdce42b8e1ce8026b71753f8f9138373
glaukiol1 <glaukiol1@gmail.com>
2022-04-10 01:23:01 +0200

    add todos for module

---
+++ module/module.go
@@ -0,0 +1,15 @@
+package module
+
+// TODO
+// module package will export many things,
+// but the most important one will be:
+/*
+struct module.Module{
+	ModuleName string
+	ModuleDoc  string
+	globals    map[string]lang.Type
+	methods    []*lang.Method
+}
+*/
+
+// the stblib will be written in Go
