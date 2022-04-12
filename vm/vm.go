3eda385fb40727c5401c92b5dace38ca2d0f8630
glaukiol1 <glaukiol1@gmail.com>
2022-04-12 16:21:17 +0200

    remove some done TODOs

--- vm/vm.go
+++ vm/vm.go
@@ -12,19 +12,6 @@ import (
 // vm | this package contains the virtual machine
 // for running gago code
 
-// TODO
-// there is a virtual memory map
-// which keeps track of variables and other things
-
-// TODO
-// there is a way to load modules,
-// and make their globals accessable
-
-// TODO
-// runs the AST, and is able to parse
-// and run the according functions
-// corresponding to the AST
-
 // The VM struct will be the holder of all the
 // subcomponents of the VM
 type VM struct {
