9ba396feffa09b0d45ffcf51cc0c56b2bf9caba5
glaukiol1 <glaukiol1@gmail.com>
2022-04-14 16:26:35 +0200

    add a cutomtype

---
+++ lang/customtype.go
@@ -0,0 +1,14 @@
+package lang
+
+// custom type is a type where it would be specific to one function
+// or created within the Gago script
+
+// this type will be used for arrays, struct-like types and more
+
+type CustomType struct {
+	BaseType
+}
+
+func LoadCustomType(qualname string, value interface{}, name string) CustomType {
+	return CustomType{BaseType: BaseType{Typename: qualname, Value: value}}
+}
